1cb0e709989f06a2c8d82648254dd75f2c2794f8
havbon <haavard.bones@gmail.com>
2019-12-31 21:52:33 +0100

    change desc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.1.1",
     author="Håvard Bønes",
     author_email="haavard.bones@gmail.com",
-    description="A colored game engine for python.",
+    description="A retro colored game engine for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/havbon/python-colored-game-engine",
